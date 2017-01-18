6d098a120a22434422682d62eebbde7c6766058c
andrewhill <andrew.hill15@imperial.ac.uk>
2017-01-18 15:35:13 +0000

    Added maven skeleton with empty java class

---
+++ src/main/java/AlexaRequest.java
@@ -0,0 +1,14 @@
+/**
+ * Created by andrewhill on 18/01/2017.
+ */
+
+import com.google.gson.Gson;
+public class AlexaRequest {
+
+    public static void main(String[] args) {
+
+        
+
+    }
+
+}
